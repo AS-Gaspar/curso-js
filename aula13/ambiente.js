d390172dab5d6c18ee167ed1dce4b50da2d2f72f
Alexandre S <airtontunni@gmail.com>
2024-08-20 12:38:45 -0300

    learning debug

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+/*var c = 1
+while (c <= 50) {
+    console.log(`${c}`)
+    c++
+}*/
+
+/*var d = 5
+do {
+    console.log(`${d}`)
+    d++
+} while (d <= 10)*/
+
---
+++ aula13/node_f4d73a6e36bf5.tmp.js
@@ -0,0 +1,10 @@
+var c = 1
+while (c <= 50) {
+    console.log(`${c}`)
+    c++
+}
+
+var d = 5
+do {
+    (`{d}`)
+} while (d <= 10)
